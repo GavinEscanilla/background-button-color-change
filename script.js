0220f1a9745480d56d9da3e4fc541dcf10ebffff
GavinEscanilla <gavinaltonescanilla@gmail.com>
2023-12-29 13:31:37 -0500

    added buttons and logic to change bg color

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const redbutton = document.getElementById('red');
+const greenbutton = document.getElementById('green');
+const bluebutton = document.getElementById('blue');
+const yellowbutton = document.getElementById('yellow');
 
+function changered() {
+    document.body.style.backgroundColor = "red";
+}
+function changegreen() {
+    document.body.style.backgroundColor = "green";
+}
+function changeblue() {
+    document.body.style.backgroundColor = "blue";
+}
+function changeyellow() {
+    document.body.style.backgroundColor = "yellow";
+}
